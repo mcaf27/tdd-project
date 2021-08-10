04d2484c9c547ebf9bf14f2963151fee7d10b030
mcaf27 <claraferr27@gmail.com>
2021-08-09 21:21:35 -0300

    Não Salvar Items em Branco a cada Requisição

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,10 @@ from lists.models import Item
 
 class HomePageTest(TestCase):
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
