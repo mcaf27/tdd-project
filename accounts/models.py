814aa69a515c13d39a4b1ced786f1f390b0aff6d
mcaf27 <claraferr27@gmail.com>
2021-08-13 23:55:27 -0300

    Modelo Token

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
@@ -8,3 +9,6 @@ class User(models.Model):
     is_anonymous = False
     is_authenticated = True
 
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
