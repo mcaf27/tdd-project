e0f4ecc284e84fd8b406a66731d2e73d7ae5f617
mcaf27 <claraferr27@gmail.com>
2021-08-16 10:27:55 -0300

    Teste funcional passando

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -14,6 +14,12 @@ class UserModelTest(TestCase):
         user = User(email='a@b.com')
         self.assertEqual(user.pk, 'a@b.com')
 
+    def test_no_problem_with_auth_login(self):
+        user = User.objects.create(email='edith@example.com')
+        user.backend = ''
+        request = self.client.request().wsgi_request
+        auth.login(request, user)  # should not raise
+
 class TokenModelTest(TestCase):
 
     def test_links_user_with_auto_generated_uid(self):
